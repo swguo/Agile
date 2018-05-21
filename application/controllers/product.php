c9ee4a8d6ac70bb53f460804897e0f93d0247dab
swguo <swguo@gm.pu.edu.tw>
2018-05-21 20:16:03 +0800

    merge user2 product view

--- application/controllers/product.php
+++ application/controllers/product.php
@@ -4,20 +4,21 @@ class Product extends CI_Controller {
 
 	public function __construct() {
 		parent:: __construct();		
-		$this->load->model('Product_model');	
+		$this->load->model('product_model');	
 	}
 
 	// 產品介紹-前台
 	public function index() {	
-		$result = $this->Product_model->getbackend();
+		$result = $this->product_model->getbackend();
 		$this->load->view('header');
 		$this->load->view('product/index', array("result"=>$result));
 		$this->load->view('footer');		
 	}
-	// 產品介紹-後台
-	public function backIndex() {		
+	public function backIndex()
+	{		
+		$result = $this->product_model->getbackend();
 		$this->load->view('back_header');
-		$this->load->view('product/b_index');
+		$this->load->view('product/b_index',array("result"=>$result));
 	}
 	public function edit(){
 		$this->load->helper('url');
