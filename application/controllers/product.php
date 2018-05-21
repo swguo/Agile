3b34970fc8f55a0a3c6660ea69c4dffbb30f28b8
user2 <user2@example.com>
2018-05-21 19:45:48 +0800

    product view

--- application/controllers/product.php
+++ application/controllers/product.php
@@ -4,21 +4,19 @@ class Product extends CI_Controller {
 
 	public function __construct() {
 		parent:: __construct();		
-		$this->load->model('product_model');	
+		$this->load->model('Product_model');	
 	}
 
 	// 產品介紹-前台
-	public function index()
-	{	
-			//$this->load->view('header');
-			$this->load->view('product/index');
-			//$this->load->view('footer');		
+	public function index() {	
+		$result = $this->Product_model->getbackend();
+		$this->load->view('header');
+		$this->load->view('product/index', array("result"=>$result));
+		$this->load->view('footer');		
 	}
 	// 產品介紹-後台
-	public function backIndex()
-	{		
-		$result = $this->product_model->getbackend();
+	public function backIndex() {		
 		$this->load->view('back_header');
-		$this->load->view('product/b_index',array("result"=>$result));
+		$this->load->view('product/b_index');
 	}
 }
