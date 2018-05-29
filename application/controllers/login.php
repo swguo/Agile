34aad970598e359bb222959c62f7347d5b15616f
swguo <swguo@gm.pu.edu.tw>
2018-05-29 17:31:46 +0800

    login view design

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 	class Login extends CI_Controller{
 		public function __construct(){
@@ -59,16 +58,4 @@
 			redirect(site_url("/login")); //轉回登入頁
 		}
 	}
-=======
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-	class Login extends CI_Controller{
-		public function __construct(){
-			parent:: __construct();
-		}
-		
-		public function index(){
-			$this->load->view('login');
-		}
-	}
->>>>>>> origin/User7
 ?>
